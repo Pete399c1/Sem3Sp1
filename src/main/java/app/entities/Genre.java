2d4b026c96a548d7932fe8c7702b8aeaa5945c7b
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-09-16 22:56:38 +0200

    added genre entity

---
+++ src/main/java/app/entities/Genre.java
@@ -0,0 +1,22 @@
+package app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
