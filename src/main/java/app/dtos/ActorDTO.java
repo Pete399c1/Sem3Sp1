b09eecbb0e2d1132671305d46c3719c8ab8d33b6
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-09-16 14:57:43 +0200

    Added ActorDTO class

---
+++ src/main/java/app/dtos/ActorDTO.java
@@ -0,0 +1,19 @@
+package app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ActorDTO {
+    private int id;
+    private String name;
+    private LocalDate BirthDate;
+}
